fb04b961f583577be488555afe4bb7c2c00c9c39
Rzncrul <38758702+Rzncrul@users.noreply.github.com>
2018-04-27 00:03:25 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,21 @@
- const Discord = require('discord.js');
-  const client = new Discord.Client();
-  
-  client.on('ready', () => {
-      console.log('I am ready!');
-  });
-  
-  client.on('message', message => 
-  {
-      if (message.content === 'touch') {
-      	message.reply('Kyaaaaa...!Captain is a pervert!Reporting to headquarters.');
-    	}
-      if (message.content === 'hug'){
-      	message.reply('W-WHO SAID U CAN HUG ME?');
- -  	}
- +    if (message.isMentioned(client.user)) {
- +    message.reply('Nani?');
- +}
-  });
-  
-  // THIS  MUST  BE  THIS  WAY
-+      client.login(process.env.BOT_TOKEN);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'touch') {
+    	message.reply('Kyaaa!');
+  	}
+    if (message.content === 'hug') {
+    	message.reply('W-WHO SAID YOU CAN TOUCH ME?');
+  	}
+      if (message.isMentioned(client.user)) {
+    message.reply('Nani?');
+}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
