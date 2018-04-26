3e91efed01384b1bfa529cce55008ce7b1e98f02
Rzncrul <38758702+Rzncrul@users.noreply.github.com>
2018-04-26 23:47:14 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,15 @@
- const Discord = require('discord.js');
- const client = new Discord.Client();
- 
- client.on('ready', () => {
-     console.log('I am ready!');
- });
- 
- client.on('message', message => {
-     if (message.content === 'touch') {
-     	message.reply('Kyaaaaa...!Captain is a pervert!Reporting to headquarters.');
-   	}
-+    if (message.content === 'hug') {
-+    	message.reply('W-WHO SAID U CAN HUG ME?');
-+  	}
- });
- 
- // THIS  MUST  BE  THIS  WAY
- client.login(process.env.BOT_TOKEN);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
