72f6233c65b6daa394e2b645c331b6c8d60c312c
Rzncrul <38758702+Rzncrul@users.noreply.github.com>
2018-04-26 22:10:33 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'touch') {
     	message.reply('Kyaaaaa...!Captain is a pervert!Reporting to headquarters.');
   	}
+    if (message.content === 'hug') {
+    	message.reply('W-WHO SAID U CAN HUG ME?');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
