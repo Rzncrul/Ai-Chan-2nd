6f26b691d6f4ad58359b8a09f5aef2b408ddd291
Rzncrul <38758702+Rzncrul@users.noreply.github.com>
2018-04-26 23:24:55 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,21 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    var str = message.content;
-    var str1 = str.toLowerCase();
-    if (str1 == 'touch') {
-    	message.reply('Kyaaaaa...!Captain is a pervert!Reporting to headquarters.');
-  	}
-    if (str1 == 'hug') || (message.content === 'Hug') {
-    	message.reply('W-WHO SAID U CAN HUG ME?');
-    }
-    if (message.isMentioned(client.user)) {
-    message.reply('Nani?');
-}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+ const client = new Discord.Client();
+ 
+ client.on('ready', () => {
+     console.log('I am ready!');
+ });
+ 
+ client.on('message', message => {
+     if (message.content === 'touch') {
+     	message.reply('Kyaaaaa...!Captain is a pervert!Reporting to headquarters.');
+   	}
+     if (message.content === 'hug') || (message.content === 'Hug'){
+     	message.reply('W-WHO SAID U CAN HUG ME?');
+-  	}
++    }
++    if (message.isMentioned(client.user)) {
++    message.reply('Nani?');
++}
+ });
+ 
+ // THIS  MUST  BE  THIS  WAY
